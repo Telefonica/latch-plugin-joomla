ec02a16fd18a919882303f6335b6b30dd5e11960
Roberto - phproberto <roberto@phproberto.com>
2014-04-11 17:19:37 +0200

    [imp] change Joomla components license to GPL + global cleanup

--- plugins/user/latch/twoFactorForm.php
+++ plugins/user/latch/twoFactorForm.php
@@ -1,22 +1,13 @@
 <?php
-/*
-Latch Joomla extension - Integrates Latch into the Joomla authentication process.
-Copyright (C) 2013 Eleven Paths
+/**
+ * @package     Latch
+ * @subpackage  Plugin
+ *
+ * @copyright   Copyright (C) 2013-2014 Eleven Paths. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+defined('_JEXEC') or die;
 ?>
 <html>
     <head>
